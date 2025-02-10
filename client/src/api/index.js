eaee0c89942c143b4173a61b1d86fdf3cb853fae
MoodyCoding <yashrajsamani@gmail.com>
2025-02-10 19:35:58 +0530

    Client deployed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://js-mastery-mern-project.onrender.com/",
+});
 
 // const url = "http://localhost:5000/posts";
 // // url pointing to our backend route
